bae7185a37145d06a04f997c74624d17aa3b9297
Shiislaja <janko.cholvadt@gmail.com>
2025-09-26 19:15:44 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,23 +1,25 @@
+// pridaj si p5.js a p5.svg.js (https://github.com/zenozeng/p5.js-svg)
+
 let particles = [];
 
 function setup() {
-  // SVG renderer umožní exportovať do vektora
-  createCanvas(800, 800, SVG);
-  background(255);
+  createCanvas(800, 800, SVG); // SVG renderer
   strokeWeight(0.6);
   noFill();
 }
 
 function draw() {
-  // prekresľovať netreba background, nechajme stopy
+  background(255);
+
+  // update & draw particles
   for (let p of particles) {
     p.update();
     p.display();
   }
 }
 
-// keď ťaháš myšou, pridávajú sa nové "vlákna"
 function mouseDragged() {
+  // vytvorenie novej "siete" vlákien pri pohybe myši
   for (let i = 0; i < 8; i++) {
     particles.push(new Particle(mouseX, mouseY));
   }
@@ -27,22 +29,22 @@ class Particle {
   constructor(x, y) {
     this.pos = createVector(x, y);
     this.vel = p5.Vector.random2D().mult(random(1, 3));
-    this.life = 120;
+    this.life = 200;
   }
 
   update() {
     this.pos.add(this.vel);
-    this.vel.mult(0.96); // spomalenie
+    this.vel.mult(0.98); // trocha spomalenie
     this.life--;
   }
 
   display() {
-    stroke(150, 0, 200, map(this.life, 0, 120, 255, 50));
+    stroke(150, 0, 200, map(this.life, 0, 200, 255, 0));
     point(this.pos.x, this.pos.y);
   }
 }
 
-// stlačením "s" uložíš SVG
+// ak chceš uložiť do SVG, stlač klávesu 's'
 function keyPressed() {
   if (key === 's') {
     save("fibers.svg");
