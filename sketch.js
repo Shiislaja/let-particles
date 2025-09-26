8c9a5315a30509d51405c865149d8e4a53eb1420
Shiislaja <janko.cholvadt@gmail.com>
2025-09-26 18:04:21 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,21 +1,50 @@
+let particles = [];
+
 function setup() {
+  // SVG renderer umožní exportovať do vektora
   createCanvas(800, 800, SVG);
   background(255);
-  stroke(200, 0, 200);
-  strokeWeight(2);
+  strokeWeight(0.6);
+  noFill();
 }
 
 function draw() {
-  // nič sa nedeje v draw
+  // prekresľovať netreba background, nechajme stopy
+  for (let p of particles) {
+    p.update();
+    p.display();
+  }
 }
 
+// keď ťaháš myšou, pridávajú sa nové "vlákna"
 function mouseDragged() {
-  // pri ťahaní myšou kreslí čiary
-  line(pmouseX, pmouseY, mouseX, mouseY);
+  for (let i = 0; i < 8; i++) {
+    particles.push(new Particle(mouseX, mouseY));
+  }
 }
 
+class Particle {
+  constructor(x, y) {
+    this.pos = createVector(x, y);
+    this.vel = p5.Vector.random2D().mult(random(1, 3));
+    this.life = 120;
+  }
+
+  update() {
+    this.pos.add(this.vel);
+    this.vel.mult(0.96); // spomalenie
+    this.life--;
+  }
+
+  display() {
+    stroke(150, 0, 200, map(this.life, 0, 120, 255, 50));
+    point(this.pos.x, this.pos.y);
+  }
+}
+
+// stlačením "s" uložíš SVG
 function keyPressed() {
   if (key === 's') {
-    save("test.svg");
+    save("fibers.svg");
   }
 }
