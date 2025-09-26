2b7b4d4b3b763ca6b219fc30d425ddb5440cf1b1
Shiislaja <janko.cholvadt@gmail.com>
2025-09-26 19:16:31 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,47 +1,21 @@
-let particles = [];
-
 function setup() {
-  createCanvas(800, 800, SVG); // SVG renderer
-  strokeWeight(0.6);
-  noFill();
+  createCanvas(800, 800, SVG);
+  background(255);
+  stroke(200, 0, 200);
+  strokeWeight(2);
 }
 
 function draw() {
-  background(255);
-
-  for (let p of particles) {
-    p.update();
-    p.display();
-  }
+  // nič sa nedeje v draw
 }
 
 function mouseDragged() {
-  for (let i = 0; i < 8; i++) {
-    particles.push(new Particle(mouseX, mouseY));
-  }
-}
-
-class Particle {
-  constructor(x, y) {
-    this.pos = createVector(x, y);
-    this.vel = p5.Vector.random2D().mult(random(1, 3));
-    this.life = 200;
-  }
-
-  update() {
-    this.pos.add(this.vel);
-    this.vel.mult(0.98);
-    this.life--;
-  }
-
-  display() {
-    stroke(150, 0, 200, map(this.life, 0, 200, 255, 0));
-    point(this.pos.x, this.pos.y);
-  }
+  // pri ťahaní myšou kreslí čiary
+  line(pmouseX, pmouseY, mouseX, mouseY);
 }
 
 function keyPressed() {
   if (key === 's') {
-    save("fibers.svg");
+    save("test.svg");
   }
 }
