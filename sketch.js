76aa8656e920a1deaa0ff59b3b289aad426b1b37
Shiislaja <janko.cholvadt@gmail.com>
2025-09-27 16:17:02 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,50 +1,61 @@
-let particles = [];
+let curves = [];
 
 function setup() {
-  // SVG renderer umožní exportovať do vektora
-  createCanvas(800, 800, SVG);
+  createCanvas(1920, 1080, SVG); // 16:9 plátno
   background(255);
-  strokeWeight(0.6);
   noFill();
+  frameRate(30);
+
+  // na začiatku pridáme pár kriviek
+  for (let i = 0; i < 40; i++) {
+    curves.push(new Swirl(width/2, height/2));
+  }
 }
 
 function draw() {
-  // prekresľovať netreba background, nechajme stopy
-  for (let p of particles) {
-    p.update();
-    p.display();
+  // nečistíme background, aby ostávali stopy
+  for (let c of curves) {
+    c.update();
+    c.display();
+  }
+
+  // priebežne pridávaj nové krivky pre nekonečný pohyb
+  if (frameCount % 10 === 0) {
+    curves.push(new Swirl(width/2, height/2));
   }
 }
 
-// keď ťaháš myšou, pridávajú sa nové "vlákna"
-function mouseDragged() {
-  for (let i = 0; i < 8; i++) {
-    particles.push(new Particle(mouseX, mouseY));
-  }
-}
-
-class Particle {
+// trieda pre vírivú krivku
+class Swirl {
   constructor(x, y) {
-    this.pos = createVector(x, y);
-    this.vel = p5.Vector.random2D().mult(random(1, 3));
-    this.life = 120;
+    this.start = createVector(x, y);
+    this.cp1 = p5.Vector.random2D().mult(random(200, 600)).add(this.start);
+    this.cp2 = p5.Vector.random2D().mult(random(200, 600)).add(this.start);
+    this.end = p5.Vector.random2D().mult(random(300, 800)).add(this.start);
+    this.life = int(random(120, 300));
+    this.weight = random(0.3, 2.5);
+    this.col = color(random(50, 200), 0, random(100, 255), 180);
   }
 
   update() {
-    this.pos.add(this.vel);
-    this.vel.mult(0.96); // spomalenie
     this.life--;
   }
 
   display() {
-    stroke(150, 0, 200, map(this.life, 0, 120, 255, 50));
-    point(this.pos.x, this.pos.y);
+    stroke(this.col);
+    strokeWeight(this.weight);
+    bezier(
+      this.start.x, this.start.y,
+      this.cp1.x, this.cp1.y,
+      this.cp2.x, this.cp2.y,
+      this.end.x, this.end.y
+    );
   }
 }
 
-// stlačením "s" uložíš SVG
+// uloženie vektorového SVG
 function keyPressed() {
   if (key === 's') {
-    save("fibers.svg");
+    save("swirl-fibers.svg");
   }
 }
