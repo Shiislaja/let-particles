e1b0ac8a833f3b3e35d54087e7dcdfef95473825
Shiislaja <janko.cholvadt@gmail.com>
2025-09-27 16:35:24 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,15 +1,15 @@
 let fibers = [];
 let center;
+let radius;
 
 function setup() {
   createCanvas(1080, 1080, SVG);
   background(255);
 
-  // stredová pozícia
   center = createVector(width / 2, height / 2);
+  radius = width * 0.45; // veľkosť kruhu
 
-  // inicializácia vlákien zo stredu
-  for (let i = 0; i < 200; i++) {
+  for (let i = 0; i < 150; i++) {
     fibers.push(new Fiber(center.x, center.y));
   }
 
@@ -23,9 +23,9 @@ function draw() {
     f.display();
   }
 
-  // pridávaj nové vlákna zo stredu postupne
-  if (frameCount % 30 === 0) {
-    for (let i = 0; i < 20; i++) {
+  // pridávaj nové vlákna zo stredu
+  if (frameCount % 40 === 0) {
+    for (let i = 0; i < 15; i++) {
       fibers.push(new Fiber(center.x, center.y));
     }
   }
@@ -36,22 +36,21 @@ class Fiber {
     this.pos = createVector(x, y);
     this.prev = this.pos.copy();
     this.life = int(random(150, 400));
-    this.weight = random(0.5, 2.5);
-    this.col = color(30, 150, 60, 150); // zelená (tráva)
+    this.weight = random(2, 5); // hrubšie linky
+    this.col = color(30, 150, 60, 180); // zelená tráva
     this.noff = createVector(random(1000), random(1000));
   }
 
   update() {
     this.prev = this.pos.copy();
 
-    // Perlin noise riadi smer expanzie
     let angle = noise(this.noff.x, this.noff.y) * TWO_PI * 2;
     let step = p5.Vector.fromAngle(angle);
-    step.mult(2); // rýchlosť rozťahovania
+    step.mult(2.5);
     this.pos.add(step);
 
-    // jemný drift od centra
-    let dir = p5.Vector.sub(this.pos, center).normalize().mult(0.5);
+    // jemný drift od stredu
+    let dir = p5.Vector.sub(this.pos, center).normalize().mult(0.8);
     this.pos.add(dir);
 
     this.noff.add(0.01, 0.01);
@@ -59,14 +58,17 @@ class Fiber {
   }
 
   display() {
-    stroke(this.col);
-    strokeWeight(this.weight);
-    line(this.prev.x, this.prev.y, this.pos.x, this.pos.y);
+    // Kreslí len ak bod leží vo vnútri kruhu
+    if (p5.Vector.dist(this.pos, center) < radius) {
+      stroke(this.col);
+      strokeWeight(this.weight);
+      line(this.prev.x, this.prev.y, this.pos.x, this.pos.y);
+    }
   }
 }
 
 function keyPressed() {
   if (key === 's') {
-    save("organic-expansion.svg");
+    save("organic-circle.svg");
   }
 }
