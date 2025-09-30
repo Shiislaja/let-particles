c96fb071522f62d937b2578244a90a0a85b8df36
Shiislaja <janko.cholvadt@gmail.com>
2025-09-30 11:44:19 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,22 +1,23 @@
 let fibers = [];
 let center;
 let radius;
+let ringRadius;
 
 function setup() {
   createCanvas(1080, 1080, SVG);
   background(255);
 
   center = createVector(width / 2, height / 2);
-  radius = width * 0.45; // hlavný polomer kruhu
+  radius = width * 0.45;
+  ringRadius = radius * 0.5;
 
-  for (let i = 0; i < 150; i++) {
+  for (let i = 0; i < 60; i++) {
     fibers.push(new Fiber(center.x, center.y));
   }
 
   noFill();
   strokeCap(ROUND);
 
-  // nakresli text až navrch
   drawTextRing();
 }
 
@@ -26,8 +27,9 @@ function draw() {
     f.display();
   }
 
-  if (frameCount % 40 === 0) {
-    for (let i = 0; i < 15; i++) {
+  // pomalšie pridávanie nových vlákien
+  if (frameCount % 80 === 0 && fibers.length < 300) {
+    for (let i = 0; i < 10; i++) {
       fibers.push(new Fiber(center.x, center.y));
     }
   }
@@ -37,9 +39,9 @@ class Fiber {
   constructor(x, y) {
     this.pos = createVector(x, y);
     this.prev = this.pos.copy();
-    this.life = int(random(150, 400));
-    this.weight = random(2, 5);
-    this.col = color(30, 150, 60, 180); // zelená tráva
+    this.life = int(random(80, 200)); // kratšia životnosť
+    this.weight = random(2, 4);
+    this.col = color(30, 150, 60, 160);
     this.noff = createVector(random(1000), random(1000));
   }
 
@@ -47,15 +49,12 @@ class Fiber {
     this.prev = this.pos.copy();
 
     let angle = noise(this.noff.x, this.noff.y) * TWO_PI * 2;
-    // malá symetrická odchýlka
     angle += random(-0.05, 0.05);
 
-    let step = p5.Vector.fromAngle(angle);
-    step.mult(2.5);
+    let step = p5.Vector.fromAngle(angle).mult(2.5);
     this.pos.add(step);
 
-    // drift od stredu
-    let dir = p5.Vector.sub(this.pos, center).normalize().mult(0.8);
+    let dir = p5.Vector.sub(this.pos, center).normalize().mult(0.6);
     this.pos.add(dir);
 
     this.noff.add(0.01, 0.01);
@@ -63,7 +62,12 @@ class Fiber {
   }
 
   display() {
-    if (p5.Vector.dist(this.pos, center) < radius) {
+    let d = p5.Vector.dist(this.pos, center);
+
+    // 1) kresli len ak si vo vonkajšom kruhu
+    // 2) vynechaj zónu prstenca s textom
+    let padding = 40;
+    if (d < radius && (d < ringRadius - padding || d > ringRadius + padding)) {
       stroke(this.col);
       strokeWeight(this.weight);
       line(this.prev.x, this.prev.y, this.pos.x, this.pos.y);
@@ -74,22 +78,27 @@ class Fiber {
 function drawTextRing() {
   let word = "POSTDIGITAL";
   let chars = word.split("");
-  let ringRadius = radius * 0.5; // druhý prstenec
 
   textAlign(CENTER, CENTER);
-  textSize(60); // môžeš doladiť podľa mierky
-  fill(30, 150, 60); // rovnaká zelená
+  textSize(60);
+  fill(30, 150, 60);
+  noStroke();
 
   for (let i = 0; i < chars.length; i++) {
     let angle = map(i, 0, chars.length, 0, TWO_PI) - HALF_PI;
     let x = center.x + cos(angle) * ringRadius;
     let y = center.y + sin(angle) * ringRadius;
-    text(chars[i], x, y);
+
+    push();
+    translate(x, y);
+    rotate(angle + HALF_PI); // otočí znak, aby sledoval kružnicu
+    text(chars[i], 0, 0);
+    pop();
   }
 }
 
 function keyPressed() {
   if (key === 's') {
-    save("postdigital-circle.svg");
+    save("postdigital-circle-optimized.svg");
   }
 }
